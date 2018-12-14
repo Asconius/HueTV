25e3193554ce533545eec3ed72d739f51aab6057
Asconius <joern.krusche@outlook.com>
2018-12-14 16:32:12 +0100

    BootUpReceiver added

---
+++ app/src/main/java/com/asconius/philipshueandroidtv/BootUpReceiver.java
@@ -0,0 +1,15 @@
+package com.asconius.philipshueandroidtv;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootUpReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.w("onReceive", "---------------------------------------------------------------------");
+        Log.w("onReceive", "onReceive");
+    }
+}
