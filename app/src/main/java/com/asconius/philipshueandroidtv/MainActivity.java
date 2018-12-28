00ae73e36328b9abe75968d46f95e8edca3921a7
joern <joern.krusche@outlook.com>
2018-12-28 12:29:08 +0100

    Image to Bitmap conversion

--- app/src/main/java/com/asconius/philipshueandroidtv/MainActivity.java
+++ app/src/main/java/com/asconius/philipshueandroidtv/MainActivity.java
@@ -3,6 +3,7 @@ package com.asconius.philipshueandroidtv;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
@@ -10,15 +11,15 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+
+import java.nio.ByteBuffer;
 
 public class MainActivity extends Activity {
 
@@ -35,18 +36,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        EventBus.getDefault().register(this);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.w("onActivityResult", "MainActivity.onActivityResult");
         mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
     }
 
     @Override
@@ -71,11 +67,30 @@ public class MainActivity extends Activity {
                 public void onImageAvailable(ImageReader reader) {
                     Image image = reader.acquireNextImage();
                     Log.w("onImageAvailable", "MainActivity.OnImageAvailableListener.onImageAvailable " + image);
+                    Bitmap bitmap = convert(image);
                     image.close();
+                    reader.close();
                 }
             }, null);
             createVirtualDisplay();
         }
+    }
+
+    public Bitmap convert(Image image) {
+        if(image == null) return null;
+        int width = image.getWidth();
+        int height = image.getHeight();
+        final Image.Plane[] planes = image.getPlanes();
+        final ByteBuffer buffer = planes[0].getBuffer();
+        int pixelStride = planes[0].getPixelStride();
+        int rowStride = planes[0].getRowStride();
+        int rowPadding = rowStride - pixelStride * width;
+        Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+        bitmap.copyPixelsFromBuffer(buffer);
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height);
+        Log.w("convert", "MainActivity.convert " + bitmap);
+        image.close();
+        return bitmap;
     }
 
     private VirtualDisplay createVirtualDisplay() {
