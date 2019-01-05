887c2eaa3a3e2c394314dba3bb80a1327d2a7e4a
joern <joern.krusche@outlook.com>
2019-01-05 13:18:05 +0100

    HueTest update

--- app/src/test/java/com/asconius/philipshueandroidtv/HueTest.java
+++ app/src/test/java/com/asconius/philipshueandroidtv/HueTest.java
@@ -8,51 +8,58 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class HueTest {
 
+    private static final int HUE_MAX = 65535;
+    private static final int HUE_MIN = 0;
+    private static final int SATURATION_MAX = 254;
+    private static final int SATURATION_MIN = 0;
+    private static final int BRIGHTNESS_MAX = 254;
+    private static final int BRIGHTNESS_MIN = 0;
+
     @Test
     public void red() {
         Hue hue = new Hue(Color.RED);
-        assertThat(hue.getHue()).isEqualTo(0);
-        assertThat(hue.getSaturation()).isEqualTo(254);
-        assertThat(hue.getBrightness()).isEqualTo(254);
+        assertThat(hue.getHue()).isEqualTo(HUE_MIN);
+        assertThat(hue.getSaturation()).isEqualTo(SATURATION_MAX);
+        assertThat(hue.getBrightness()).isEqualTo(BRIGHTNESS_MAX);
     }
 
     @Test
     public void blue() {
         Hue hue = new Hue(Color.BLUE);
-        assertThat(hue.getHue()).isEqualTo(43690);
-        assertThat(hue.getSaturation()).isEqualTo(254);
-        assertThat(hue.getBrightness()).isEqualTo(254);
+        assertThat(hue.getHue()).isEqualTo(HUE_MAX / 3 * 2);
+        assertThat(hue.getSaturation()).isEqualTo(SATURATION_MAX);
+        assertThat(hue.getBrightness()).isEqualTo(BRIGHTNESS_MAX);
     }
 
     @Test
     public void green() {
         Hue hue = new Hue(Color.GREEN);
-        assertThat(hue.getHue()).isEqualTo(21845);
-        assertThat(hue.getSaturation()).isEqualTo(254);
-        assertThat(hue.getBrightness()).isEqualTo(254);
+        assertThat(hue.getHue()).isEqualTo(HUE_MAX / 3);
+        assertThat(hue.getSaturation()).isEqualTo(SATURATION_MAX);
+        assertThat(hue.getBrightness()).isEqualTo(BRIGHTNESS_MAX);
     }
 
     @Test
     public void yellow() {
         Hue hue = new Hue(Color.YELLOW);
-        assertThat(hue.getHue()).isEqualTo(10922);
-        assertThat(hue.getSaturation()).isEqualTo(254);
-        assertThat(hue.getBrightness()).isEqualTo(254);
+        assertThat(hue.getHue()).isEqualTo(HUE_MAX / 6);
+        assertThat(hue.getSaturation()).isEqualTo(SATURATION_MAX);
+        assertThat(hue.getBrightness()).isEqualTo(BRIGHTNESS_MAX);
     }
 
     @Test
     public void white() {
         Hue hue = new Hue(Color.WHITE);
-        assertThat(hue.getHue()).isEqualTo(0);
-        assertThat(hue.getSaturation()).isEqualTo(0);
-        assertThat(hue.getBrightness()).isEqualTo(254);
+        assertThat(hue.getHue()).isEqualTo(HUE_MIN);
+        assertThat(hue.getSaturation()).isEqualTo(SATURATION_MIN);
+        assertThat(hue.getBrightness()).isEqualTo(BRIGHTNESS_MAX);
     }
 
     @Test
     public void black() {
         Hue hue = new Hue(Color.BLACK);
-        assertThat(hue.getHue()).isEqualTo(0);
-        assertThat(hue.getSaturation()).isEqualTo(0);
-        assertThat(hue.getBrightness()).isEqualTo(0);
+        assertThat(hue.getHue()).isEqualTo(HUE_MIN);
+        assertThat(hue.getSaturation()).isEqualTo(SATURATION_MIN);
+        assertThat(hue.getBrightness()).isEqualTo(BRIGHTNESS_MIN);
     }
 }
